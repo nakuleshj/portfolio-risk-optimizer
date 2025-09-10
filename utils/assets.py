805a46a983acca5b0bcd2d94649f184c24f26ff8
Nakulesh J. <nakuleshj1998@gmail.com>
2025-09-10 05:59:43 -0400

    Add summary statistics calculations

--- utils/assets.py
+++ utils/assets.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import scipy.stats as s
 
 def get_price_data(tickers: list, period: str ):
     prices = yf.Tickers(tickers=tickers).history(period=period)
@@ -21,6 +22,28 @@ def drawdowns(returns):
     cum_returns = (1+returns).cumprod()
     running_max = np.maximum.accumulate(cum_returns)
     return (cum_returns - running_max)/running_max
+
+def summary_stats(returns, rf=0.02):
+    d_vol = returns.std()
+    ann_vol = returns.std() * np.sqrt(252)
+    avg_ret = returns.mean()
+    ann_avg_ret = (1+avg_ret) ** 252 - 1
+    skew = s.skew(returns)
+    kurtosis = s.kurtosis(returns)
+    stats = pd.DataFrame(
+        {
+            "Avg. Daily Return": returns.mean(),
+            "Avg. Annual Return": ((1+returns.mean()) ** 252-1),
+            "Daily Volatility": d_vol,
+            "Annualized Volatility": ann_vol,
+            "Skew": skew,
+            "Excess Kurtosis": kurtosis,
+            "Sharpe Ratio": (ann_avg_ret - rf) / ann_vol
+        },
+        index=returns.columns.values
+    )
+    return stats.T
+
 
 p = get_price_data(['AAPL','TSLA'],'1y')
 r = calc_returns(p)
